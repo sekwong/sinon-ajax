504345582a2c54dbc90105ed851f4bb1851ecb2b
Teamzoon <tz-dev1@Teamzoons-iMac.local>
2017-02-28 15:05:56 +0700

    unit test for validate data

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var expect = chai.expect;
 describe('MyAPI', function () {
     var xhr;
     var requests;
+    // set up fake xhr
     beforeEach(function () {
         xhr = sinon.useFakeXMLHttpRequest();
         requests = [];
@@ -10,7 +11,25 @@ describe('MyAPI', function () {
         };
     });
 
-    afterEach(function() {
+    it('should parse fetched data as JSON', function (done) {
+        var data = { foo: 'bar' };
+        var dataJson = JSON.stringify(data);
+        myapi.get(function (err, result) {
+            expect(result).to.deep.equal(data);
+            done();
+        });
+        // response with fake data from fak xhr
+        requests[0].respond(200, { 'Content-Type': 'text/json' }, dataJson);
+    });
+
+    it('should send given data as JSON body', function () {
+        var data = { hello: 'world' };
+        var dataJson = JSON.stringify(data);
+        myapi.post(data, function () { });
+        expect(requests[0].requestBody).to.equal(dataJson);
+    });
+
+    afterEach(function () {
         xhr.restore();
     });
 });
